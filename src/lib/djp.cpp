701040bd87c9e90eb130a860220d979411b579d4
Dani Berg <dani@daniberg.com>
2012-04-11 23:40:11 -0700

    We parse the command line now

--- src/lib/djp.cpp
+++ src/lib/djp.cpp
@@ -1,3 +1,14 @@
-int main(int argc, char **argv) {
+#include <iostream>
+#include "CmdInput.h"
+using namespace djp;
+
+int main(int argc, const char **argv) {
+  CmdInput ci;
+  if (ci.processCmdArgs(argc, argv)) {
+    std::cerr << "Error: " << ci.error << std::endl;
+    return 1;
+  }
+
+  std::cout << "Work in progress" << std::endl;
   return 0;
 }
