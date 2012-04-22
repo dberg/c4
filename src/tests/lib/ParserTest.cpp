169126e2da74807b03aeb0f80f9b72a852db6b6f
Dani Berg <dani@daniberg.com>
2012-04-21 22:42:12 -0700

    Add gtest

---
+++ src/tests/lib/ParserTest.cpp
@@ -0,0 +1,14 @@
+#include "Output.h"
+#include "Parser.h"
+#include "gtest/gtest.h"
+using namespace djp;
+
+// TODO:
+TEST(Parser, PackageDeclaration) {
+  ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
