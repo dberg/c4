c3c9c96a2972cdd4b055e83cb7c317fd9f81dee9
dberg <dberg@kabam.com>
2012-04-30 23:07:08 -0700

    First unit test

--- src/tests/lib/ParserTest.cpp
+++ src/tests/lib/ParserTest.cpp
@@ -1,11 +1,22 @@
+#include <iostream>
 #include "Output.h"
 #include "Parser.h"
 #include "gtest/gtest.h"
 using namespace djp;
 
-// TODO:
 TEST(Parser, PackageDeclaration) {
-  ASSERT_EQ(1, 1);
+  std::string filename = "Test.java";
+  std::string buffer = "@myinterface\npackage com.test;";
+  Parser parser(filename, buffer);
+  parser.parse();
+  ASSERT_EQ(1, parser.compilationUnit->pkgDecl->annotations.size());
+  ASSERT_EQ(0, parser.compilationUnit->pkgDecl->annotations[0]->posTokAt);
+  ASSERT_EQ(1, parser.compilationUnit->pkgDecl->annotations[0]->qualifiedId
+    ->pos);
+  ASSERT_EQ(11, parser.compilationUnit->pkgDecl->annotations[0]->qualifiedId
+    ->len);
+  ASSERT_EQ(1, parser.compilationUnit->pkgDecl->annotations[0]->qualifiedId
+    ->identifiers[0]->pos);
 }
 
 int main(int argc, char **argv) {
