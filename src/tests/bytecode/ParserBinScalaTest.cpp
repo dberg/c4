f5a6f39d086fa79c7236dc42f515c4c382e67aa8
dberg <dberg@kabam.com>
2013-08-20 19:33:49 -0700

    Initial tests parsing scala bytecode.

--- src/tests/bytecode/ParserBinScalaTest.cpp
+++ src/tests/bytecode/ParserBinScalaTest.cpp
@@ -18,6 +18,32 @@ TEST(ParserScalaBin, HelloWorld) {
   ParserBin parser(filename, buffer);
   parser.parse();
 
+  ASSERT_EQ(0xCAFEBABE, parser.classFile->magic);
+  ASSERT_EQ(0, parser.classFile->minor_version);
+  ASSERT_EQ(50, parser.classFile->major_version);
+  ASSERT_EQ(45, parser.classFile->constant_pool_count);
+  ASSERT_EQ(45, parser.classFile->constant_pool->items.size());
+
+  {
+    // Item 1: CONSTANT_Utf8
+    spCPItem item = parser.classFile->constant_pool->items[1];
+    ASSERT_EQ(CONSTANT_Utf8, item->tag);
+    spCUtf8Info cUtf8Info = item->cUtf8Info;
+    ASSERT_EQ(10, item->cUtf8Info->length);
+    ASSERT_EQ(10, item->cUtf8Info->bytes.size());
+    std::string str(item->cUtf8Info->bytes.begin(),
+      item->cUtf8Info->bytes.end());
+    ASSERT_EQ("HelloWorld", str);
+  }
+
+  {
+    // Item 2: CONSTANT_Class HelloWorld
+    spCPItem item = parser.classFile->constant_pool->items[2];
+    ASSERT_EQ(CONSTANT_Class, item->tag);
+    spCClassInfo cClassInfo = item->cClassInfo;
+    ASSERT_EQ(1, cClassInfo->name_index);
+  }
+
   // TODO: HelloWorld$.class
   // TODO: HelloWorld$delayedInit$body.class
 }
