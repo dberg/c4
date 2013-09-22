36bafd84904ae88b575d68ac1a70cd6c91676040
dberg <dberg@kabam.com>
2013-09-22 09:30:56 -0700

    Fix scala syntax highlighting unit tests.

--- src/tests/scala/ScalaSyntaxHighlightingTest.cpp
+++ src/tests/scala/ScalaSyntaxHighlightingTest.cpp
@@ -16,17 +16,17 @@ TEST(ScalaSyntaxHighlighting, HelloWorld) {
 
   std::string expected =
     "["
-    "(c4-sh-keyword 1 7)"           // object
-    "(c4-sh-identifier 8 18)"       // HelloWorld
-    "(c4-sh-keyword 19 26)"         // extends
-    "(c4-sh-identifier 27 30)"      // App
-    "(c4-sh-op 31 32)"              // {
-    "(c4-sh-identifier 33 40)"      // println
-    "(c4-sh-op 40 41)"              // (
-    "(c4-sh-string-literal 41 54)"  // "Hello world
-    "(c4-sh-op 54 55)"              // )"
-    "(c4-sh-op 55 56)"              // ;
-    "(c4-sh-op 57 58)"              // }
+    "(c4s-sh-keyword 1 7)"           // object
+    "(c4s-sh-identifier 8 18)"       // HelloWorld
+    "(c4s-sh-keyword 19 26)"         // extends
+    "(c4s-sh-identifier 27 30)"      // App
+    "(c4s-sh-op 31 32)"              // {
+    "(c4s-sh-identifier 33 40)"      // println
+    "(c4s-sh-op 40 41)"              // (
+    "(c4s-sh-string-literal 41 54)"  // "Hello world
+    "(c4s-sh-op 54 55)"              // )"
+    "(c4s-sh-op 55 56)"              // ;
+    "(c4s-sh-op 57 58)"              // }
     "]";
 
   ASSERT_EQ(expected, output.sh);
@@ -47,22 +47,22 @@ TEST(ScalaSyntaxHighlighting, Trait) {
 
   std::string expected =
     "["
-    "(c4-sh-keyword 1 8)"        // package
-    "(c4-sh-identifier 9 13)"    // test
-    "(c4-sh-keyword 14 20)"      // import
-    "(c4-sh-identifier 21 24)"   // com
-    "(c4-sh-op 24 25)"           // .
-    "(c4-sh-identifier 25 32)"   // company
-    "(c4-sh-op 32 33)"           // .
-    "(c4-sh-identifier 33 38)"   // utils
-    "(c4-sh-op 38 39)"           // .
-    "(c4-sh-op 39 40)"           // _
-    "(c4-sh-keyword 41 46)"      // trait
-    "(c4-sh-identifier 47 48)"   // X
-    "(c4-sh-keyword 49 56)"      // extends
-    "(c4-sh-identifier 57 58)"   // Y
-    "(c4-sh-keyword 59 63)"      // with
-    "(c4-sh-identifier 65 66)"   // Z
+    "(c4s-sh-keyword 1 8)"        // package
+    "(c4s-sh-identifier 9 13)"    // test
+    "(c4s-sh-keyword 14 20)"      // import
+    "(c4s-sh-identifier 21 24)"   // com
+    "(c4s-sh-op 24 25)"           // .
+    "(c4s-sh-identifier 25 32)"   // company
+    "(c4s-sh-op 32 33)"           // .
+    "(c4s-sh-identifier 33 38)"   // utils
+    "(c4s-sh-op 38 39)"           // .
+    "(c4s-sh-op 39 40)"           // _
+    "(c4s-sh-keyword 41 46)"      // trait
+    "(c4s-sh-identifier 47 48)"   // X
+    "(c4s-sh-keyword 49 56)"      // extends
+    "(c4s-sh-identifier 57 58)"   // Y
+    "(c4s-sh-keyword 59 63)"      // with
+    "(c4s-sh-identifier 65 66)"   // Z
     "]";
 
   ASSERT_EQ(expected, output.sh);
