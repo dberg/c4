b25334a20e5dfd5a656e9e3a0adf4ddc6806bf96
Dani Berg <dani@daniberg.com>
2013-10-11 19:45:42 -0700

    Remove hard coded values in token enums.

--- src/scala/include/c4/ScalaToken.h
+++ src/scala/include/c4/ScalaToken.h
@@ -7,62 +7,62 @@
 namespace c4s {
 
 enum class STok : int {
-  ERROR = -1,
-  END_OF_FILE = -2,
+  ERROR,
+  END_OF_FILE,
 
   // Reserved words
-  ABSTRACT = -3,
-  CASE = -4,
-  CATCH = -5,
-  CLASS = -6,
-  DEF = -7,
-  DO = -8,
-  ELSE = -9,
-  EXTENDS = -10,
-  FALSE = -11,
-  FINAL = -12,
-  FINALLY = -13,
-  FOR = -14,
-  FORSOME = -15,
-  IF = -16,
-  IMPLICIT = -17,
-  IMPORT = -18,
-  LAZY = -19,
-  MATCH = -20,
-  NEW = -21,
-  NULL_KEY = -22, // null
-  OBJECT = -23,
-  OVERRIDE = -24,
-  PACKAGE = -25,
-  PRIVATE = -26,
-  PROTECTED = -27,
-  RETURN = -28,
-  SEALED = -29,
-  SUPER = -30,
-  THIS = -31,
-  THROW = -32,
-  TRAIT = -33,
-  TRY = -34,
-  TRUE = -35,
-  TYPE = -36,
-  VAL = -37,
-  VAR = -38,
-  WHILE = -39,
-  WITH = -40,
-  YIELD = -41,
-  UNDERSCORE = -42, // _
-  COLON = -43, // :
-  EQUALS = -44, // =
-  EQUALS_GT = -45, // =>
-  LT_DASH = -46, // <-
-  LT_COLON = -47, // <:
-  LT_PERCENTAGE = -48, // <%
-  GT_COLON = -49, // >:
-  HASH = -50, // #
-  AT = -51, // @
+  ABSTRACT,
+  CASE,
+  CATCH,
+  CLASS,
+  DEF,
+  DO,
+  ELSE,
+  EXTENDS,
+  FALSE,
+  FINAL,
+  FINALLY,
+  FOR,
+  FORSOME,
+  IF,
+  IMPLICIT,
+  IMPORT,
+  LAZY,
+  MATCH,
+  NEW,
+  NULL_KEY, // null
+  OBJECT,
+  OVERRIDE,
+  PACKAGE,
+  PRIVATE,
+  PROTECTED,
+  RETURN,
+  SEALED,
+  SUPER,
+  THIS,
+  THROW,
+  TRAIT,
+  TRY,
+  TRUE,
+  TYPE,
+  VAL,
+  VAR,
+  WHILE,
+  WITH,
+  YIELD,
+  UNDERSCORE, // _
+  COLON, // :
+  EQUALS, // =
+  EQUALS_GT, // =>
+  LT_DASH, // <-
+  LT_COLON, // <:
+  LT_PERCENTAGE, // <%
+  GT_COLON, // >:
+  HASH, // #
+  AT, // @
 
   // Lexer grammar
-  ID = -52,
+  ID,
   ESCAPE_SEQUENCE,
 
   // Literals
@@ -77,18 +77,24 @@ enum class STok : int {
   OCTAL_NUMERAL_WITH_INT_TYPE_SUFFIX,
 
   // Symbol Terminals
-  COMMA = -53, // ,
-  PERIOD = -54, // .
-  SEMICOLON = -55, // ;
-  LCURLYB = -56, // {
-  RCURLYB = -57, // }
-  LPAREN = -58, // (
-  RPAREN = -59, // )
-  LBRACKET = -60, // [
-  RBRACKET = -61, // ]
+  COMMA, // ,
+  PERIOD, // .
+  SEMICOLON, // ;
+  LCURLYB, // {
+  RCURLYB, // }
+  LPAREN, // (
+  RPAREN, // )
+  LBRACKET, // [
+  RBRACKET, // ]
 
-  MINUS = -62, // -
-  PLUS = -63, // +
+  // Operators
+  MINUS,
+  PLUS,
+  DIV,
+  MUL,
+  SLASH_EQUALS,
+
+  COMMENT,
 };
 
 class ScalaTokenUtil {
