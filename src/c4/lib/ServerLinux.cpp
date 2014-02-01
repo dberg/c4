46e71d9ce283bd4eb4a1d17028fa999f2cce242b
dberg <dberg@kabam.com>
2014-02-01 14:57:28 -0800

    Initial work on linux server.

--- src/c4/lib/ServerLinux.cpp
+++ src/c4/lib/ServerLinux.cpp
@@ -14,7 +14,61 @@ int Server::start(CmdInput &ci) {
     return -1;
   }
 
-  // TODO:
+  // monitor listening socket
+  struct epoll_event ev;
+  ev.events = EPOLLIN;
+  ev.data.fd = listenfd;
+  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &ev) < 0) {
+    errMsg = "failed to monitor listening socket.";
+    return -1;
+  }
+
+  const int MAX_EVENTS = 1024;
+  struct epoll_event events[MAX_EVENTS];
+  struct sockaddr_in cliaddr;
+
+  while (true) {
+    int nevs = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+    for (int i = 0; i < nevs; i++) {
+      // listening socket - accept new connections
+      if (events[i].data.fd == listenfd) {
+        socklen_t clilen = sizeof(cliaddr);
+        int connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
+        if (connfd < 0) {
+          std::cerr << "Failed to accept connection." << std::endl;
+          continue;
+        }
+
+        // create message buffer
+        createMessageBuffer(connfd);
+
+        // monitor new connection
+        ev.events = EPOLLIN;
+        ev.data.fd = connfd;
+        if (epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &ev) < 0) {
+          std::cerr << "Failed to monitor new connection fd#"
+            << connfd << std::endl;
+        }
+        continue;
+      }
+
+      // handle socket communication
+      // read data
+      //if (evlist[i].flags & EVFILT_READ) {
+        //int cbytes = read(evlist[i].ident, readBuffer, READ_BUFFER_MAX);
+        //feed(evlist[i].ident, readBuffer, cbytes);
+
+        //if (cbytes < 0) {
+          // TODO:
+          // read error
+        //} else {
+          // TODO:
+          // send data to MessageBuffer
+        //}
+      //}
+    }
+  }
+
   return 0;
 }
 
