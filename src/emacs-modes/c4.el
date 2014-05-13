9a77fc0c1032cab84ee8bbaddd4c5b9e516b7c18
dberg <dberg@kabam.com>
2014-05-12 18:56:00 -0700

    Initial work on c4 compile request.

--- src/emacs-modes/c4.el
+++ src/emacs-modes/c4.el
@@ -1,29 +1,37 @@
-;; c4 server communication.
+;; server settings
 (defvar c4-server-host "localhost"
   "Default hostname of the c4 server")
 
 (defvar c4-server-port 8000
   "Default port of the c4 server")
 
+;; server communication
 (defvar c4-process-object-name "c4-process-object"
   "Default name of a c4 server connection")
+
+;; request types
+(defconst request-action-project 0)
+(defconst request-action-compile 1)
 
 (defvar c4-process-object nil
   "Connection to the c4 server")
 
-(defun c4-connection-alivep ()
-  (not (process-command c4-process-object)))
-
+;; TODO: handle connection errors
 (defun c4-connect-to-server ()
   "Connect to a c4 server using 'c4-server-object'."
+  (interactive)
   (let ((name c4-process-object-name)
         (buffer "c4-buffer-output") ;; should be nil and use a filter
         (host c4-server-host)
         (service c4-server-port))
-    (if (c4-connection-alivep)
+    (if (c4-connection-alivep c4-process-object)
         c4-process-object
       (setq c4-process-object
             (open-network-stream name buffer host service)))))
+
+(defun c4-connection-alivep (process-object)
+  (and (processp process-object)
+       (not (process-command process-object))))
 
 (defun c4-write-request (request)
   "Send a request object to a c4 server"
@@ -37,5 +45,11 @@
 
 (defun c4-create-request-compile (project-id)
   "Create a request to compile a list of compilation units."
-  ;; TODO
-  )
+  (let ((request
+         '(:action request-action-compile
+           :project-id project-id
+           :compilation-unit (:filename TODO.java
+                              :buffer "public class A {}"))))
+    request))
+
+(provide 'c4)
