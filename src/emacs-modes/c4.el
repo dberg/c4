5a6b928a89ecf6633a3e4dc34115b519f1e748de
dberg <dberg@kabam.com>
2014-05-14 18:25:56 -0700

    Initial work serializing request in the client.

--- src/emacs-modes/c4.el
+++ src/emacs-modes/c4.el
@@ -10,8 +10,8 @@
   "Default name of a c4 server connection")
 
 ;; request types
-(defconst request-action-project 0)
-(defconst request-action-compile 1)
+(defconst c4-request-action-project 0)
+(defconst c4-request-action-compile 1)
 
 (defvar c4-process-object nil
   "Connection to the c4 server")
@@ -29,6 +29,25 @@
       (setq c4-process-object
             (open-network-stream name buffer host service)))))
 
+(defun c4-compile-current-buffer ()
+  "Send the current buffer to a c4 server."
+  (interactive)
+  (let* ((project-id (c4-get-project-id))
+         (filename (buffer-file-name))
+         (buffer (buffer-substring-no-properties 1 (point-max)))
+         (request (c4-create-request-compile
+                   project-id
+                   filename
+                   buffer)))
+    ;; TODO:
+    ;;(c4-write-request request)
+    (c4-write-request buffer)))
+
+(defun c4-get-project-id ()
+  "Get the project id of the current buffer"
+  ;; TODO
+  "project-001")
+
 (defun c4-connection-alivep (process-object)
   (and (processp process-object)
        (not (process-command process-object))))
@@ -36,20 +55,34 @@
 (defun c4-write-request (request)
   "Send a request object to a c4 server"
   ;; TODO
-  (process-send-string c4-process-object (byte-to-string 65)))
+  (process-send-string c4-process-object request))
 
 (defun c4-create-request-project (project-id)
   "Create a request to get information about the project."
   ;; TODO
   )
 
-(defun c4-create-request-compile (project-id)
+(defun c4-create-request-compile (project-id filename buffer)
   "Create a request to compile a list of compilation units."
   (let ((request
          '(:action request-action-compile
            :project-id project-id
-           :compilation-unit (:filename TODO.java
-                              :buffer "public class A {}"))))
-    request))
+           :compilation-unit (:filename filename
+                              :buffer buffer))))
+    (c4-serialize-request request)))
+
+
+(defconst c4-protobuf-varint 0)
+(defconst c4-protobuf-64bit 1)
+(defconst c4-protobuf-length-delimited 2)
+(defconst c4-protobuf-32bit 5)
+
+(defun c4-serialize-request (request)
+  "Serialize a request using protocol buffers"
+  ;; TODO:
+  (let (action project-id unit)
+    (setq action (logior (lsh c4-request-action-compile 3)
+                         c4-protobuf-length-delimited))
+    "TODO"))
 
 (provide 'c4)
