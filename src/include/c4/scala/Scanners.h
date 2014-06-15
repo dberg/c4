76181d973531c0daf457b2eab55dae61debf1a3f
dberg <dberg@kabam.com>
2014-06-15 09:08:23 -0700

    Add sepRegions to Scanner class.

--- src/include/c4/scala/Scanners.h
+++ src/include/c4/scala/Scanners.h
@@ -2,6 +2,8 @@
 #ifndef __C4_SCALA_SCANNERS_H__
 #define __C4_SCALA_SCANNERS_H__
 
+#include <vector>
+
 #include "c4/scala/ScannersTypeDefs.h"
 #include "c4/scala/Tokens.h"
 
@@ -29,7 +31,22 @@ UnitScanner
 
 class TokenData {
 protected:
+
   Token token;
+
+  /** a stack of tokens which indicates whether line-ends can be statement separators
+   *  also used for keeping track of nesting levels.
+   *  We keep track of the closing symbol of a region. This can be
+   *  RPAREN    if region starts with '('
+   *  RBRACKET  if region starts with '['
+   *  RBRACE    if region starts with '{'
+   *  ARROW     if region starts with `case'
+   *  STRINGLIT if region is a string interpolation expression starting with '${'
+   *            (the STRINGLIT appears twice in succession on the stack iff the
+   *             expression is a multiline string literal).
+   */
+  std::vector<Token> sepRegions;
+
 public:
   TokenData() : token(EMPTY) {}
 };
