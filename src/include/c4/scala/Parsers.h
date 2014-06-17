825bf7fd372b28f02ddea9db80febacc09528ffc
dberg <dberg@kabam.com>
2014-06-17 08:53:59 -0700

    Initial work on parser classes.

--- src/include/c4/scala/Parsers.h
+++ src/include/c4/scala/Parsers.h
@@ -2,6 +2,8 @@
 #ifndef __C4_SCALA_PARSERS_H__
 #define __C4_SCALA_PARSERS_H__
 
+#incude "c4/scala/ParsersTypeDefs.h"
+
 namespace c4s {
 
 /* Notes:
@@ -9,12 +11,24 @@ namespace c4s {
 ParserCommon
 Parser
 SourceFileParser
-ParserUnit
+UnitParser
 
 // CompilationUnit ::= {package QualId semi} TopStatSeq
 def compilationUnit(): PackageDef
 
 */
+
+class Parser : public ParserCommon {
+
+};
+
+class SourceFileParser : public Parser {
+
+};
+
+class UnitParser : public SourceFileParser {
+
+};
 
 } // namespace
 
---
+++ src/include/c4/scala/ParsersTypeDefs.h
@@ -0,0 +1,14 @@
+//-*- C++ -*-
+#ifndef __C4_SCALA_PARSERS_TYPE_DEFS_H__
+#define __C4_SCALA_PARSERS_TYPE_DEFS_H__
+
+#include <memory>
+
+namespace c4s {
+
+class UnitParser;
+typedef std::shared_ptr<UnitParser> spUnitParser;
+
+} // namespace
+
+#endif
