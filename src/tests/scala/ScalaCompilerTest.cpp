4d09aff86a53d942e3a822fb8c0b082cec4162b7
dberg <dberg@kabam.com>
2014-07-04 09:26:53 -0700

    Add first scala compiler unit test.

--- src/tests/scala/ScalaCompilerTest.cpp
+++ src/tests/scala/ScalaCompilerTest.cpp
@@ -1,8 +1,35 @@
+#include <string>
+#include <vector>
+
 #include "gtest/gtest.h"
+
+#include "c4/scala/SourceFile.h"
 #include "c4/scala/Global.h"
+#include "utf8/utf8.h"
 
 using namespace c4s;
 
 TEST(ScalaCompiler, PackageDef) {
+
+  std::string filename = "A.scala";
+  std::string sourceCode =
+    "object A {"
+    "  def main(args: Array[String]) = {"
+    "    println(\"Hello world\")"
+    "  }"
+    "}";
+
+  std::vector<Char> buffer;
+  utf8::utf8to16(sourceCode.begin(), sourceCode.end(),
+    std::back_inserter(buffer));
+
+  spClientSourceFile source = spClientSourceFile(new ClientSourceFile(
+    filename, buffer));
+  spCompilationUnit unit = spCompilationUnit(new CompilationUnit(source));
+
+  std::vector<spCompilationUnit> units;
+  units.push_back(unit);
+
   spGlobal global = spGlobal(new Global);
+  global->compile(units);
 }
