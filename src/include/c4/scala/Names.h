8dafabdad56f2205858a41cbcb42a23e72da2df3
dberg <dberg@kabam.com>
2014-07-16 18:07:42 -0700

    Add Names class files.

---
+++ src/include/c4/scala/Names.h
@@ -0,0 +1,10 @@
+//-*- C++ -*-
+#ifndef __C4_SCALA_NAMES_H__
+#define __C4_SCALA_NAMES_H__
+
+namespace c4s {
+
+
+} // namespace
+
+#endif
---
+++ src/lib/scala/Names.cpp
@@ -0,0 +1,5 @@
+#include "c4/scala/Names.h"
+
+namespace c4s {
+
+} // namespace
