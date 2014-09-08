0587541f7adbe74306a1278a398ad7ec3eadabb5
dberg <dberg@kabam.com>
2014-09-07 17:41:53 -0700

    Update Printers.

--- src/include/c4/scala/Printers.h
+++ src/include/c4/scala/Printers.h
@@ -8,6 +8,16 @@
 namespace c4s {
 
 class Printers {
+private:
+
+  // TODO: see Printers::show.
+  std::string render(Tree *tree);
+
+  // TODO: additional parameters are
+  //       printTypes, printIds, printOwners, printKinds,
+  //       printMirrors and printPositions.
+  // We'll skip that for now.
+  std::string show(Tree *tree);
 public:
   Printers();
   std::string treeToString(Tree *tree);
--- src/lib/scala/Printers.cpp
+++ src/lib/scala/Printers.cpp
@@ -5,9 +5,17 @@ namespace c4s {
 /** Constructor */
 Printers::Printers() {}
 
-std::string Printers::treeToString(Tree *tree) {
-  // TODO:
+std::string Printers::render(Tree *tree) {
+  // TODO: Print missing parameters. See Printers::show.
   return "TODO";
+}
+
+std::string Printers::show(Tree *tree) {
+  return render(tree);
+}
+
+std::string Printers::treeToString(Tree *tree) {
+  return show(tree);
 }
 
 } // namespace
