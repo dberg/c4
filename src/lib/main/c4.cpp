2cdf8a022196868eeb622f913b28537d08295724
dberg <dberg@kabam.com>
2015-10-20 18:51:16 -0700

    Initial work parsing cmd line args.

--- src/lib/main/c4.cpp
+++ src/lib/main/c4.cpp
@@ -3,8 +3,16 @@
 #include "c4/main/CmdInput.h"
 
 using namespace c4;
+using std::cerr;
+using std::endl;
 
 int main(int argc, const char** argv) {
   CmdInput ci(argc, argv);
+  if (ci.processCmdArgs()) {
+    cerr << "Error: " << ci.getError() << endl;
+    return 1;
+  }
+
+
   return 0;
 }
